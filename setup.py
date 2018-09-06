d64ffcd9ca796f1169f7358d6589afee165e46c9
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2018-09-06 16:15:35 +0200

    update reqs

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
     description='Framework for modeling and generating test datasets',
     author='Tor Solli-Nowlan',
     version='0.1.dev0',
+    install_requires=[
+        "python-dateutil>=2.7.2",
+        "datetime"
+    ],
     packages=['parhelion',],
     license='AGPLv3',
 )
