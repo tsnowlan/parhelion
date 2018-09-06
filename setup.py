46686c13b158e60faa1310bd08b614967fcdbb97
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2018-09-06 14:39:45 +0200

    setuptools not distutils

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Parhelion',
     description='Framework for modeling and generating test datasets',
     author='Tor Solli-Nowlan',
-    version='0.1dev',
+    version='0.1.dev0',
     packages=['parhelion',],
     license='AGPLv3',
 )
